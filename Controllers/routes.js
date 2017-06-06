b08447658f70c96ad05f253d8c537b1125a69c45
Steven Fox <sfrunner1188@gmail.com>
2017-06-06 14:42:32 -0700

    Able to Retrieve Links & Title, Check for Unique Values, and Send to DB

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -3,19 +3,59 @@ var router = express.Router();
 var cheerio = require("cheerio");
 var request = require("request");
 
+var mongoose = require('mongoose');
+
+//Package to Check DB for Unique Values
+var uniqueValidator = require('mongoose-unique-validator');
+mongoose.connect('mongodb://localhost/articlescraping');
+mongoose.plugin(uniqueValidator); 
+
+var Article = require("../Models/articleScraping.js")
 
 
 router.get("/", function (req, res) {
     request("https://www.wsj.com", function (error, response, html) {
-        //console.log(html);
         var $ = cheerio.load(html);
-        $(".lead-story").children().children().children().children().children().children().each(function (i, val) {
-            if (val.children[0] != null) {
-                if (val.children[0].type === "text") {
+        //Right Stories
+        $(".lead-story").children().children().children().each(function (i, val) {
+            if (val.children[0].children[0] != null) {
+                if (val.children[0].children[0].name === "a") {
                     console.log("Link");
-                    console.log(val.children[0]);
+                    console.log(val.children[0].children[0].attribs.href);
                     console.log("Data Summary");
-                    console.log(val.children[0].data);
+                    console.log(val.children[0].children[0].children[0].data);
+
+                    //Send Data to MongoDB via Mongoose
+                    var article = new Article({ title: val.children[0].children[0].children[0].data, link: val.children[0].children[0].attribs.href });
+                    article.save(function (err) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("Product Inserted");
+                        }
+                    });
+                }
+            }
+        });
+
+        //Left & Bottom Stories
+        $(".lead-story").children().children().children().children().each(function (i, val) {
+            if (val.children[0].children[0] != null) {
+                if (val.children[0].children[0].name === "a") {
+                    console.log("Link");
+                    console.log(val.children[0].children[0].attribs.href);
+                    console.log("Data Summary");
+                    console.log(val.children[0].children[0].children[0].data);
+
+                    //Send Data to MongoDB via Mongoose
+                    var article = new Article({ title: val.children[0].children[0].children[0].data, link: val.children[0].children[0].attribs.href });
+                    article.save(function (err) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("Product Inserted");
+                        }
+                    });
                 }
             }
         });
---
+++ Models/articleScraping.js
@@ -0,0 +1,5 @@
+﻿var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/articlescraping');
+
+var Article = mongoose.model("Article", { title: { type: String, unique: true }, link: { type: String, unique: true } });
+module.exports = Article;
